f7923bb9b9e13d0c495a029f6b3ffb7d7eebb2bc
Simon Balleux <simoncestmoi@hotmail.fr>
2021-05-12 14:56:25 +0200

    Structure HTML CSS page inscription

---
+++ Views/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+
+
+
+<script>
+    export default {
+        data() {
+            return {
+                title: ""
+            }
+        }
+    }
+</script>
