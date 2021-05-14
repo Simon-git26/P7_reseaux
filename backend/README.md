18ea0da1bd12cc1a38316ecda2a073222f6f7475
Simon Balleux <simoncestmoi@hotmail.fr>
2021-05-14 16:33:38 +0200

    Vue CLI/ Node / Bootstrap/Structure Backend/Frontend

--- backend/README.md
+++ backend/README.md
@@ -5,3 +5,4 @@
 initialisation de Node avec npm init sur la backend
 Installation de Nodemon avec npm install -g nodemon
 Instalation de express avec npm install express --save 
+Installation de mysql : npm install mysql
--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,37 @@ const express = require('express');
 //Méthode qui permet de crée une appli express
 const app = express();
 
+
+//Importer Mysql
+const mysql = require('mysql2');
+
+
+//------------------------Connection mysql bdd=projet7-------------------------
+//Connection mysql
+const bdd = mysql.createConnection({
+    database: process.env.BDD,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+});
+
+bdd.connect(function(err) {
+    if (err) throw err;
+    console.log('Connecté à la base de données mySQL')
+});
+
+
+
+//Middleware CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+
 // Middleware
 app.use((req, res, next) => {
     console.log('Requete recue !');
