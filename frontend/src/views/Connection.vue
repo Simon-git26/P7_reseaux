9439ea9a36960639d85c318b4c4a952c04344217
Simon Balleux <simoncestmoi@hotmail.fr>
2021-05-17 11:54:39 +0200

    Page Inscription/Connection faite + Components Header Fait/Relier

---
+++ frontend/src/views/Connection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="connection">
+    <Header />
+  </div>
+</template>
+
+
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Connection',
+  components: {
+    Header //Preciser que je veux ce composant Header
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
