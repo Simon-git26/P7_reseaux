3c4ebbe4962a5913869b8e3371f069bcf040d37d
Simon Balleux <simoncestmoi@hotmail.fr>
2021-05-25 09:30:14 +0200

    Frontend components Login OK

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,30 +1,107 @@
 <template>
-    <form>
-        <h3>Connection</h3>
+    <div>
+        <p v-if="password && email ">
+            <b>Le mot de passe ne correspond pas à l'email renseigné</b>
+        </p>
 
-        <div class="form-group">
-            <label>Email</label>
-            <input type="email" class="form-control" placeholder="Email" />
-        </div>
+        <form @submit.prevent="handlelogsubmit, checkFormLog">
+            <h3>Connection</h3>
 
-        <div class="form-group">
-            <label>Mot de Passe</label>
-            <input type="password" class="form-control" placeholder="Mot de passe" />
-        </div>
+            <div class="form-group">
+                <label>Email *</label>
+                <input type="email" class="form-control" v-model="email" placeholder="Email" required />
+            </div>
 
-        <button class="btn btn-primary btn-block">Connection</button>
+            <div class="form-group">
+                <label>Mot de Passe *</label>
+                <input type="password" class="form-control" v-model="password" placeholder="Mot de passe" required />
+            </div>
 
-        <p>Vous n'avez pas de compte ?<a href="Register">Inscrivez vous !</a></p>
-    </form>
+            <button class="btn btn-primary btn-block" :disabled="btnlogdisable">Connection</button>
+
+            <p>Vous n'avez pas de compte ?<a href="Register">Inscrivez vous !</a></p>
+        </form>
+    </div>
+    
 </template>
 
 
 
+
+
 <script>
+    import axios from '../api'
+
     export default {
-        name: 'Login'
+        name: 'Login',
+
+        data() {
+            return {
+                showErrors: [],
+                error: ["error"],
+                email: "",
+                password: ""
+            } 
+        },
+
+        methods: {
+            handlelogsubmit() {
+                const data = {
+                    email: this.email,
+                    password: this.password,
+                };
+
+                axios.post('/login', data)
+                    .then(res => {
+                        console.log(res)
+                        }
+                    ).catch(err => {
+                        console.log(err)
+                    }
+                )
+            },
+
+            checkFormLog() {
+                if (this.email && this.password) {
+                    console.log('checkFormLog fonction est appelé')
+                }
+                this.error = [];
+
+                if (!this.email) {
+                    this.error.push('Email is required')
+                }
+
+                if (!this.password) {
+                    this.error.push('password is required')
+                }
+            }
+        },
+
+
+        computed: {
+            //Ma fonction de Validation Formulaire
+            btnlogdisable: function() {
+                return this.error.length !== 0
+            }
+        },
+
+
+        watch: {
+            email(value) {
+                this.email = value;
+                this.checkFormLog();
+            },
+
+            password(value) {
+                this.password = value;
+                this.checkFormLog();
+            }
+        }
     }
 </script>
+
+
+
 
 
 <style scoped>
