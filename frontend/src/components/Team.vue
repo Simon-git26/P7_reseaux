b353c7582f51a8bb8ddcf7ed156f5abe0a4dd968
Simon Balleux <simoncestmoi@hotmail.fr>
2021-07-13 13:21:28 +0200

    Trouvez comment différencier le modo des employés

--- frontend/src/components/Team.vue
+++ frontend/src/components/Team.vue
@@ -2,13 +2,12 @@
     <div>
         <h3 class="d-flex justify-content-center mb-3">Notre Equipe</h3>
         <div class="col-6 mx-auto breakword text-break mb-4">
-            <h4>Moderateur</h4>
-            <p>fffffffffffffffffffffffffffkkkkkkkkkkkkkkkkkkkkkkkkssssssssssssssssssssssslllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll</p>
+            <h4>Modérateur</h4>
+            
         </div>
     
-        
         <div class="col-6 mx-auto">
-            <h4>Employé</h4>
+            <h4>Employés</h4>
             <div v-for="user in users" :key="user.id" :user="user" class="d-flex border border-secondary rounded-3 mb-3">
                 <div class="col-4" v-if="user.imagePath">
                     <img :src="`http://localhost:3000/${user.imagePath}`" class="img-fluid rounded-circle w-75" alt="#">
