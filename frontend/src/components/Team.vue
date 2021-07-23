cc07e0d9204e888bddbdaf53ec2deefcd718c11d
Simon Balleux <simoncestmoi@hotmail.fr>
2021-07-23 11:33:25 +0200

    Petit systeme de redirection vers boite mail

--- frontend/src/components/Team.vue
+++ frontend/src/components/Team.vue
@@ -12,7 +12,11 @@
 
                     <div class="col-md-8 d-flex align-items-center text-center">
                         <div class="card-body d-flex justify-content-between breakword text-break">
-                            <h5>{{ user.firstName }} {{ user.lastName }}<span class="ml-1" v-if="isConnected && user.isAdmin == true"><em class="fs-6 fas fa-star"></em></span></h5>
+                            <div>   
+                                <h5>{{ user.firstName }} {{ user.lastName }}<span class="ml-1" v-if="isConnected && user.isAdmin == true"><em class="fs-6 fas fa-star"></em></span></h5>
+                                <a href="mailto:">{{ user.email }}</a>
+                            </div>
+                            
                             <div v-if="isConnected && $root.user.isAdmin == true">
                                 <button class="btn-danger btn-sm ml-2" @click.prevent="modoDeleteUser(user)">
                                     <em class="fas fa-trash-alt"></em>
